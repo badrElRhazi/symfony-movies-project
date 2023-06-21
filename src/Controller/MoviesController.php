822dddf64a29b3ee93bb7e81412c180085a39068
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-21 11:40:09 +0200

    Movie Controller created

---
+++ src/Controller/MoviesController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class MoviesController extends AbstractController
+{
+    #[Route('/movies', name: 'app_movies')]
+    public function index(): Response
+    {
+        return $this->render('movies/index.html.twig', [
+            'controller_name' => 'MoviesController',
+        ]);
+    }
+}
