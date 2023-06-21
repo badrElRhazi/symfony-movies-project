9a3aecfe22f61fc37c5bed4cc43f898534d05b56
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-21 13:33:43 +0200

    old Route method

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
@@ -11,6 +12,17 @@ class MoviesController extends AbstractController
     #[Route('/movies', name: 'app_movies')]
     public function index(): Response
     {
+        return $this->render('movies/index.html.twig', [
+            'controller_name' => 'MoviesController',
+        ]);
+    }    
+    
+     /**
+      * oldMethod
+      *
+      * @Route("/old", name="old")
+      */
+     public function oldMethod(): Response{
         return $this->render('movies/index.html.twig', [
             'controller_name' => 'MoviesController',
         ]);
