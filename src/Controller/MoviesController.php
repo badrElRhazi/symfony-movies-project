56d9cb3ec176c5dc74c8b7688d31d877054a4227
Badr El Rhazi <rzi.badr@gmail.com>
2023-06-22 12:14:20 +0100

    fetching data with findBy parameters

--- src/Controller/MoviesController.php
+++ src/Controller/MoviesController.php
@@ -12,17 +12,27 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class MoviesController extends AbstractController
 {
+
+    private $em;
+    
+    public function __construct(EntityManagerInterface $em){
+        
+        $this->em = $em;
+    }
+    
     #[Route('/movies', name: 'app_movies')]
-    public function index(EntityManagerInterface $em): Response
+    public function index(): Response
     {
 
-        $repository = $em -> getRepository(Movie::class);
-        $movies =$repository -> findAll();
+        // findAll - SELECT * FROM movies:
+        // find(x) - SELECT * FROM movies where id =x
+        //findBy() - SELECT * FROM movies ORDER BY id DESC
+        $repository = $this ->em -> getRepository(Movie::class);
+        $movies =$repository -> findBy([],  ['id' => 'DESC']);
 
         dd($movies);
 
         return $this->render('movies/index.html.twig');
     }    
-    
      
 }
