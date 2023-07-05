80cf5d7f1313056e962f77261ee7325f74e66474
Badr El Rhazi <rzi.badr@gmail.com>
2023-07-05 11:57:04 +0100

    movie Form

--- src/Form/MovieFormType.php
+++ src/Form/MovieFormType.php
@@ -52,12 +52,10 @@ class MovieFormType extends AbstractType
                     ),
                     'label' => false
                 ])
-            ->add('imagePath', FileType::class,[
-                'attr' => array(
-                    'class' => 'py-10'                    
-                ),
-                'label' => false
-            ])
+                ->add('imagePath', FileType::class, array(
+                    'required' => false,
+                    'mapped' => false
+                ))
             //->add('actors')
         ;
     }
